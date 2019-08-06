bffc7d34a93e3bbfc3cedf3d2d5b75419b483d87
cp <cp@PICASSO_13>
2019-08-06 10:25:51 +0300

    first commmit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TodoItem(probs){
+    return(
+        <div>
+            title: {probs.title}
+            description:  {probs.description}
+        </div>
+    )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import TodoItem from './TodoItem';
+
+export default class TodoList extends React.Component{
+    
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        fetch('https://nztodo.herokuapp.com/api/task/?format=json')
+          .then(response => response.json())
+          .then(data => this.setState({ data }));
+      }
+
+    render(){
+        return(
+                <div>
+                {
+                    this.state.data.map(
+                        function(singleItem) {
+                            return(
+                            <TodoItem title = {singleItem.title} description = {singleItem.description}/>
+                            )
+                        }
+                    )
+                }
+                </div>
+        )
+    }
+}
